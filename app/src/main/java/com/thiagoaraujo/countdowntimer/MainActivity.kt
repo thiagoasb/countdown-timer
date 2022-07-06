9a1dfb7cb82a4d096e4b6f75e0a518556326c5c4
thiagoasb <thiagoasbezerra@gmail.com>
2022-07-05 22:16:38 -0300

    add countdown timer v1

--- app/src/main/java/com/thiagoaraujo/countdowntimer/MainActivity.kt
+++ app/src/main/java/com/thiagoaraujo/countdowntimer/MainActivity.kt
@@ -2,10 +2,32 @@ package com.thiagoaraujo.countdowntimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import com.thiagoaraujo.countdowntimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            countdownTimer()
+        }
+    }
+
+    private fun countdownTimer() {
+        object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                binding.tvTimer.text = (millisUntilFinished / 1000).toString()
+            }
+
+            override fun onFinish() {
+                binding.tvTimer.text = getString(R.string.finished)
+            }
+
+        }.start()
     }
 }

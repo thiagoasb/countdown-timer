bfb3d986c899e14bae7a88e50a55d6a45c25d105
thiagoasb <thiagoasbezerra@gmail.com>
2022-07-06 21:38:15 -0300

    add edit text field to user insert the timer in seconds

--- app/src/main/java/com/thiagoaraujo/countdowntimer/MainActivity.kt
+++ app/src/main/java/com/thiagoaraujo/countdowntimer/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.thiagoaraujo.countdowntimer
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.thiagoaraujo.countdowntimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,12 +18,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnStart.setOnClickListener {
-            countdownTimer()
+            if (!validate()) {
+                Toast.makeText(this, R.string.empty_field, Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            val timeInSeconds = binding.editTextLabel.text.toString().toLong()
+            binding.editTextLabel.setText("")
+
+            countdownTimer(timeInSeconds*1000)
+
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+
+            binding.editTextLabel.clearFocus()
         }
     }
 
-    private fun countdownTimer() {
-        object : CountDownTimer(30000, 1000) {
+    private fun countdownTimer(chosenTime: Long) {
+        object : CountDownTimer(chosenTime, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 binding.tvTimer.text = (millisUntilFinished / 1000).toString()
                 binding.btnStart.isEnabled = false
@@ -32,4 +48,10 @@ class MainActivity : AppCompatActivity() {
 
         }.start()
     }
+
+    private fun validate(): Boolean {
+        return binding.editTextLabel.text.isNotEmpty()
+                &&  !binding.editTextLabel.text.toString().startsWith("0")
+    }
+
 }
